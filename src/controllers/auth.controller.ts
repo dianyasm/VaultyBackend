061359e801156955e9c2bb7bc45531bb1a380a37
dianyasm <dianyasm@gmail.com>
2025-01-21 13:34:19 +0100

    tercer commit

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
-class AuthController{
+export class AuthController{
     static register(req:Request, res:Response){
-
+        //acceder a la bd
     }
 
     static login(req:Request, res:Response){

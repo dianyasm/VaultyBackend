c27bf57bc7ec956b769ac81776b18e3e2ed970de
dianyasm <dianyasm@gmail.com>
2025-02-19 10:10:22 +0100

    version final

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { AuthController} from '../controllers/auth.controller'
+import {AuthController} from '../controllers/auth.controller'
 import { loginValidation, registerValidation } from "../middlewares/validators.middleware";
 import { ValidationMiddleware } from "../middlewares/validation.middleware";
 const router = Router()
@@ -7,5 +7,6 @@ const router = Router()
 router.post('/login', loginValidation, ValidationMiddleware, AuthController.login)
 router.post('/logout', AuthController.logout)
 router.post('/register', registerValidation, ValidationMiddleware, AuthController.register)
+router.get('/user', AuthController.getAuthenticatedUser);
 
 export default router
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,28 +1,31 @@
 import { prisma } from "../database/database";
 import { HttpException } from "../exceptions/httpException";
-import { PrismaClient, User } from "@prisma/client";
+import { User } from "@prisma/client";
 import bcrypt from "bcrypt"
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken"
 
-const TOKEN_PASSWORD = process.env.TOKEN_PASSWORD || "pass"
+// usar un patron: singlenton
+//const prisma = new PrismaClient()
+const TOKEN_PASSWORD = process.env.TOKEN_PASSWORD || 'pass'
 
-export class AuthService{
-    static async register(user: User){
-        //ver si el usuario no existe
-        // select id,nombre from user where email=user.email
-       const findUser = await prisma.user.findUnique({where:{ email: user.email}})
-       if(findUser) throw new HttpException(409, `User ${user.email} already exists`)
-    
-        //encriptar el password
+export class AuthService {
+    static async register(user: User) {
+        // ver si el usuario no existe
+        // SELECT id,nombre FROM user WHERE email=user.email
+        const findUser = await prisma.user.findUnique({where: {email: user.email}})
+        if (findUser) throw new HttpException(409, `User ${user.email} already exists`)
+
+        // encriptar el password
         const passwordEncrypted = await bcrypt.hash(user.password, 10)
         user.password=''
-        //guardar el usuario en la bd
+        // guardar el usuario en la bd
         // INSERT INTO user (name, password, email) VALUES (?,?,?)
         return await prisma.user.create({
             data:{
                 ...user,
                 password: passwordEncrypted,
-                role: null
+                role: null,
+                //a:'aa'
             },
             omit:{
                 password:true
@@ -35,21 +38,21 @@ export class AuthService{
         //const query = `SELECT id, email, role, password FROM user WHERE email='${email}'`
         //const findUsers = await prisma.$queryRawUnsafe(query) as User[]
         //const findUser = findUsers[0]
+
         const findUser = await prisma.user.findUnique({where:{email}})
         if(!findUser) throw new HttpException(401, 'Invalid user or password')
-
-        //ver si el password coincide
+         // ver si el password coincide
         const isPasswordCorrect = await bcrypt.compare(password, findUser.password)
-        if(!isPasswordCorrect) throw new HttpException(401, 'Invalid user or password')
-        
-        //generar el token de autenticacion
+        if(!isPasswordCorrect) throw new HttpException(401,'Invalid user or password')
+
+        // generar el token de autenticación
         const token = jwt.sign(
-            {colorFavorito:"azul", id:findUser.id, email:findUser.email, role:findUser.role},
-            TOKEN_PASSWORD,
+            {colorFavorito:'azul', id:findUser.id, email:findUser.email, role:findUser.role}, 
+            TOKEN_PASSWORD, 
             {expiresIn:"1h"}
         )
-        //devolver el token
-        return token
+        // devolver el token y el usuario
+        return {token, user: { id: findUser.id, email: findUser.email, role: findUser.role } }
     }
 
 }
